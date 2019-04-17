7379259984358baef66c3b749d8998a3201a8e6d
jinbanglin <17golang@gmail.com>
2019-04-17 11:30:39 +0800

    update

--- mysql.go
+++ mysql.go
@@ -1,3 +1,22 @@
 package helper
 
+import (
+  "github.com/go-xorm/xorm"
+  "github.com/spf13/viper"
+)
 
+var engine *xorm.Engine
+
+func MYSQLInstance() *xorm.Engine {
+  if engine == nil {
+    var err error
+    engine, err = xorm.NewEngine("mysql", viper.GetString("mysql.addr"))
+    if err != nil {
+      panic(err)
+      return nil
+    }
+    engine.ShowSQL(viper.GetBool("mysql.show_log"))
+    return engine
+  }
+  return engine
+}
